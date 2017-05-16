e25bcad51198e4c2978b9a8ddeb4ecb10ab96077
valdeslab <dvjr22@gmail.com>
2017-05-16 10:25:14 -0400

    Added an example of swipe to delete

--- app/src/main/java/com/example/valdeslab/learningapp/RecyclerViewFragment.java
+++ app/src/main/java/com/example/valdeslab/learningapp/RecyclerViewFragment.java
@@ -1,18 +1,25 @@
 package com.example.valdeslab.learningapp;
 
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecyclerViewFragment extends Fragment {
 
+    private RecyclerView recyclerView;
 
-    public RecyclerViewFragment() {
-        // Required empty public constructor
-    }
+
+    public RecyclerViewFragment() {}
 
     public static RecyclerViewFragment newInstance(){
         return new RecyclerViewFragment();
@@ -22,8 +29,64 @@ public class RecyclerViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        return view;
+
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        RVAdapter rvAdapter = new RVAdapter(getData());
+        recyclerView.setAdapter(rvAdapter);
+
+        setSwipeForRecyclerView();
+    }
+
+    private List<String> getData() {
+        List<String> modelList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            modelList.add("data item : " + i);
+        }
+        return modelList;
+    }
+
+    private void setSwipeForRecyclerView() {
+
+        SwipeUtil swipeHelper = new SwipeUtil(0, ItemTouchHelper.LEFT, getActivity()) {
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                RVAdapter adapter = (RVAdapter) recyclerView.getAdapter();
+                adapter.pendingRemoval(swipedPosition);
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                RVAdapter adapter = (RVAdapter) recyclerView.getAdapter();
+                if (adapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+        };
+
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(swipeHelper);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+
+        //set swipe label
+        swipeHelper.setLeftSwipeLable("Archive");
+        //set swipe background-Color
+        swipeHelper.setLeftcolorCode(ContextCompat.getColor(getActivity(), R.color.colorRed));
+
+    }
+
+
 
 }
