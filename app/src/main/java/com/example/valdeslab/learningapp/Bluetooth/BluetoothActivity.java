f73be896309a5d520a0891422e0a35a13c61a788
valdeslab <dvjr22@gmail.com>
2017-11-02 11:18:11 -0400

    Started working on activity

--- app/src/main/java/com/example/valdeslab/learningapp/Bluetooth/BluetoothActivity.java
+++ app/src/main/java/com/example/valdeslab/learningapp/Bluetooth/BluetoothActivity.java
@@ -1,31 +1,50 @@
 package com.example.valdeslab.learningapp.Bluetooth;
 
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothHealth;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.example.valdeslab.learningapp.NavigationActivity;
 import com.example.valdeslab.learningapp.R;
+import com.example.valdeslab.learningapp.Utilities.BluetoothHDPService;
 
 import java.util.Iterator;
 import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT = 0;
-    private static final int REQUEST_DISCOVERABLE = 1;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_DISCOVERABLE = 2;
 
     private static final String TAG = "trace";
 
-    private BluetoothHealth bluetoothHealth;
 
     public static Intent newIntent(Context context) {
         return new  Intent(context, BluetoothActivity.class);
@@ -38,15 +57,6 @@ public class BluetoothActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bluetooth);
 
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            // Device doesn't support bluetooth
-        }
-
-        // check if bluetooth is enabled
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(intent, REQUEST_ENABLE_BT);
-        }
 
         // get all the bluetooth device paired to the device
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
@@ -67,12 +77,13 @@ public class BluetoothActivity extends AppCompatActivity {
 
         }
 
+/*
         // register for broadcasts when a device is discovered
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(Receiver, filter);
+*/
 
-
-        // This asks to make device discovera ble for 5 minutes
+        // This asks to make device discoverable for 5 minutes
         // default is 2 min
 /*
         Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
@@ -118,18 +129,14 @@ public class BluetoothActivity extends AppCompatActivity {
 
         String test = "request: " + requestCode + " result " + resultCode;
 
-        switch(requestCode) {
+        switch (requestCode) {
             case REQUEST_ENABLE_BT:
-                if (resultCode < 0); // Do something when request is denied
-                break;
-
-            case REQUEST_DISCOVERABLE:
-                log("Something happened");
-                break;
-
-            default:
-                startActivity(NavigationActivity.newIntent(this));
-                break;
+                if (resultCode == Activity.RESULT_OK) {
+                    // initialize();
+                } else {
+                    finish();
+                    return;
+                }
         }
 
         log(test);
