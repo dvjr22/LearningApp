67b3afbca2ca76e700ee1b1f2e9fe245cd7d2a19
valdeslab <dvjr22@gmail.com>
2017-12-26 11:08:22 -0500

    Added Simulation class and execution delay

--- app/src/main/java/com/example/valdeslab/learningapp/GoogleCharts/GoogleChartActivity.java
+++ app/src/main/java/com/example/valdeslab/learningapp/GoogleCharts/GoogleChartActivity.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.valdeslab.learningapp.NavigationActivity;
 import com.example.valdeslab.learningapp.R;
+import com.example.valdeslab.learningapp.Utilities.PatientSimulator;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class GoogleChartActivity extends AppCompatActivity {
+
+    private static final String TAG = "trace";
 
 
     /***********************************************************************************************
@@ -25,5 +33,21 @@ public class GoogleChartActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_chart);
+
+        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                simulateThatPatient();
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+    }
+
+    public void simulateThatPatient(){
+
+        Log.i(TAG, Integer.toString(PatientSimulator.heartRate()));
+
+        PatientSimulator.heartRate();
+
     }
 }
---
+++ app/src/main/java/com/example/valdeslab/learningapp/Utilities/PatientSimulator.java
@@ -0,0 +1,13 @@
+package com.example.valdeslab.learningapp.Utilities;
+
+/**
+ * Created by valdeslab on 12/26/17.
+ */
+
+public class PatientSimulator {
+
+    public static int heartRate(){
+        return 85;
+    }
+
+}
