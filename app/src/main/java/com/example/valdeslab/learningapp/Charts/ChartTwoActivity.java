835d0adf5a18818073a5bb6aa443079e0d6a6d4c
valdeslab <dvjr22@gmail.com>
2017-12-27 12:00:42 -0500

    Completed initial line chart test

--- app/src/main/java/com/example/valdeslab/learningapp/Charts/ChartTwoActivity.java
+++ app/src/main/java/com/example/valdeslab/learningapp/Charts/ChartTwoActivity.java
@@ -6,10 +6,29 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.valdeslab.learningapp.R;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ChartTwoActivity extends AppCompatActivity {
+
+    //int[] time = {0,1,2,3,4,5,6};
+    //int[] hr = {85,86,87,85,84,86,88};
+
+    ArrayList<Integer> time = new ArrayList<>();
+    ArrayList<Integer> hr = new ArrayList<>();
+
+    int count = 0;
+    int heart = 85;
 
     public static Intent newIntent(Context context){
         return new Intent(context, ChartTwoActivity.class);
@@ -21,15 +40,47 @@ public class ChartTwoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart_two);
 
-        loadLineChart();
+        execute();
     }
 
-    private void loadLineChart(){
+    private void loadLineChart(ArrayList<Integer> time, ArrayList<Integer> hr){
 
         FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = LineChartFragment.newInstance();
+        Fragment fragment = LineChartFragment.newInstance(time, hr);
         fragmentManager.beginTransaction().
-                add(R.id.chart_two_activity_fragment_container, fragment).commit();
+                replace(R.id.chart_two_activity_fragment_container, fragment).commit();
+    }
+
+
+    /***********************************************************************************************
+     *
+     */
+    private void execute() {
+
+        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                incrementData();
+                loadLineChart(time, hr);
+            }
+        }, 0, 500, TimeUnit.MILLISECONDS);
 
     }
+
+    private void incrementData(){
+        time.add(count);
+        hr.add(heart);
+
+        count++;
+
+        if (count % 2 == 0) {
+            heart++;
+        } else {
+            heart--;
+        }
+
+        Log.i("trace", Integer.toString(heart) + " " + Integer.toString(count));
+    }
+
 }
--- app/src/main/java/com/example/valdeslab/learningapp/Charts/LineChartFragment.java
+++ app/src/main/java/com/example/valdeslab/learningapp/Charts/LineChartFragment.java
@@ -3,11 +3,13 @@ package com.example.valdeslab.learningapp.Charts;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.valdeslab.learningapp.R;
+import com.example.valdeslab.learningapp.Utilities.PatientSimulator;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -15,14 +17,31 @@ import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LineChartFragment extends Fragment {
 
-    public static LineChartFragment newInstance() {
-        return new LineChartFragment();
+    private static final String BUNDLE_TIME = "bundle_time";
+    private static final String BUNDLE_HR = "bundle_hr";
+
+    LineChart lineChart;
+
+    public static LineChartFragment newInstance(ArrayList<Integer> time, ArrayList<Integer> hr) {
+
+        Bundle bundle = new Bundle();
+
+        bundle.putIntegerArrayList(BUNDLE_TIME, time);
+        bundle.putIntegerArrayList(BUNDLE_HR, hr);
+
+        LineChartFragment fragment = new LineChartFragment();
+        fragment.setArguments(bundle);
+
+        return fragment;
     }
 
     public LineChartFragment() {}
@@ -33,33 +52,29 @@ public class LineChartFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_line_chart, container, false);
 
-        LineChart lineChart = (LineChart) view.findViewById(R.id.line_chart);
+        lineChart = (LineChart) view.findViewById(R.id.line_chart);
 
-        List<Entry> data = generateData();
+        List<Entry> data = generateData(getArguments().getIntegerArrayList(BUNDLE_TIME),
+                getArguments().getIntegerArrayList(BUNDLE_HR));
 
         LineDataSet dataSet = new LineDataSet(data, "Label");
 
         LineData lineData = new LineData(dataSet);
         lineChart.setData(lineData);
-        //lineChart.invalidate();
+        lineChart.invalidate();
 
         return view;
     }
 
-
-    private List generateData(){
+    private List generateData(ArrayList<Integer> time, ArrayList<Integer> hr){
 
         List<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(1, 85));
-        entries.add(new Entry(2, 86));
-        entries.add(new Entry(3, 84));
-        entries.add(new Entry(4, 83));
-        entries.add(new Entry(5, 82));
-        entries.add(new Entry(6, 81));
+
+        for (int i = 0; i < time.size(); i++){
+            entries.add(new Entry(time.get(i), hr.get(i)));
+        }
 
         return entries;
     }
-
-
 
 }
